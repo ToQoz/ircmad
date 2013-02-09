cb94e17e3ad1939d48924c21c1efb9e832fa4ee6
Takatoshi Matsumoto <toqoz403@gmail.com>
2013-02-10 08:12:39 +0900

    Check whether posted message is valid JSON

--- lib/ircmad/irc_client.rb
+++ lib/ircmad/irc_client.rb
@@ -5,32 +5,38 @@ class Ircmad
     def initialize(&block)
       instance_eval(&block) if block_given?
 
-      unless @zircon
-        @zircon = Zircon.new config
+      @client = Zircon.new config
 
-        @zircon.send(:login) if @zircon.respond_to?(:login, true)
-        config[:channel_list].each do |channel|
-          @zircon.join channel
-        end
-      end
-      @zircon
+      @client.send(:login) if @client.respond_to?(:login, true)
+      config[:channel_list].each { |channel| @client.join channel }
     end
 
     def run!
       Ircmad.post_channel.subscribe do |msg|
-        data = JSON.parse(msg, :symbolize_names => true) rescue nil
-        privmsg data[:channel], ":#{data[:message]}"
+        parsed_msg = begin
+         JSON.parse(msg, :symbolize_names => true) rescue nil
+        rescue JSON::ParserError
+          puts "#{msg} is invalid json"
+        rescue  => e
+          puts "Unexpected error"
+          puts e.message
+          puts e.backtrace.join("\n")
+        end
+
+        if parsed_msg && parsed_msg[:channel] && parsed_msg[:message]
+          privmsg parsed_msg[:channel], ":#{parsed_msg[:message]}"
+        end
       end
 
       on_privmsg do |msg|
         Ircmad.get_channel << msg
       end
 
-      @zircon.run!
+      @client.run!
     end
 
     def method_missing(action, *args, &block)
-      @zircon.send(action.to_s, *args, &block)
+      @client.send(action.to_s, *args, &block)
     end
   end
 end
